f8562384fefc24de2b9df7953e002717f3d809ca
mavundamashaka <mavundamashaka@gmail.com>
2022-06-07 11:44:06 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - program entry point
+ * decription - print _putcher
+ * return: 0
+ */
+int main(void)
+{
+	char x[9] = "_putcher";
+	int i;
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(x[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
+
