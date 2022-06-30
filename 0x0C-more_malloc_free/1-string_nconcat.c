9796a2a809bd6b7940337919d1c55c31746eed54
mavundamashaka <mavundamashaka@gmail.com>
2022-06-30 13:11:52 -0700

    Second commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = size2;
 	}
-	
+
 	p = malloc((size1 + n + 1) * sizeof(char));
 
 	if (p == NULL)
--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -32,5 +32,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		*(relloc + i) = clone[i];
 	}
 	free(ptr);
+
 	return (relloc);
 }
